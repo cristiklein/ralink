81627dac92342e2fc433b43d0942803fe7703a7b
Cristian KLEIN <cristiklein@gmail.com>
2014-08-28 21:57:02 +0200

    Attempt new compilation fix

--- os/linux/rt_linux.c
+++ os/linux/rt_linux.c
@@ -1138,8 +1138,8 @@ static inline void __RtmpOSFSInfoChange(OS_FS_INFO * pOSFSInfo,
 		pOSFSInfo->fsgid = current->fsgid;
 		current->fsuid = current->fsgid = 0;
 #else
-		pOSFSInfo->fsuid = current_fsuid();
-		pOSFSInfo->fsgid = current_fsgid();
+		pOSFSInfo->fsuid = __kuid_val(current_fsuid());
+		pOSFSInfo->fsgid = __kgid_val(current_fsgid());
 #endif
 		pOSFSInfo->fs = get_fs();
 		set_fs(KERNEL_DS);
